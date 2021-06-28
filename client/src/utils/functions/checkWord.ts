09319f3720a26ceae4ce49f50435dfe6a13acf89
Asmit shrivastava <asmitshrivastava8@gmail.com>
2021-06-28 16:05:45 +0530

    bug: added trim() while adding the word

--- client/src/utils/functions/checkWord.ts
+++ client/src/utils/functions/checkWord.ts
@@ -1,5 +1,5 @@
 import { Word } from "../../features/words/words.types";
 
 export const checkWord = (words:Word[],word:string):boolean => {
-     return words.some( w => w.word.id.toLowerCase() === word.toLowerCase())
+     return words.some( w => w.word.id.toLowerCase() === word.toLowerCase().trim())
 }
